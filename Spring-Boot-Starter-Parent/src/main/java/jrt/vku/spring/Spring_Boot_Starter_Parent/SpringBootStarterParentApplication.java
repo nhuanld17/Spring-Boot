b5b2464f166ddf1e04282a67c98749a0e671c3b8
Le Dinh Nhuan <ledinhnhuan1917@gmail.com>
2024-08-27 01:06:05 +0700

    Learn Spring Boot Starter Parent

---
+++ Spring-Boot-Starter-Parent/src/main/java/jrt/vku/spring/Spring_Boot_Starter_Parent/SpringBootStarterParentApplication.java
@@ -0,0 +1,13 @@
+package jrt.vku.spring.Spring_Boot_Starter_Parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterParentApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Starter-Parent/src/test/java/jrt/vku/spring/Spring_Boot_Starter_Parent/SpringBootStarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package jrt.vku.spring.Spring_Boot_Starter_Parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
