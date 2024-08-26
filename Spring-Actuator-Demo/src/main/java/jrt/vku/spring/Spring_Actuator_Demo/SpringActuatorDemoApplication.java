6c9e4177e2c3bae9ed6bd1b80cb82ab292b2752a
Le Dinh Nhuan <ledinhnhuan1917@gmail.com>
2024-08-27 01:07:17 +0700

    Learn spring actuator

---
+++ Spring-Actuator-Demo/src/main/java/jrt/vku/spring/Spring_Actuator_Demo/SpringActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package jrt.vku.spring.Spring_Actuator_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorDemoApplication.class, args);
+	}
+
+}
