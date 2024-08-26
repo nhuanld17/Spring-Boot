64b928e67f8c7b56b17c1045f907930cdb1f7761
Le Dinh Nhuan <ledinhnhuan1917@gmail.com>
2024-08-27 01:07:56 +0700

    Actuator and security

---
+++ Spring-Actuator-And-Sercurity-Demo/src/main/java/jrt/vku/spring/Spring_Actuator_And_Sercurity_Demo/SpringActuatorAndSercurityDemoApplication.java
@@ -0,0 +1,13 @@
+package jrt.vku.spring.Spring_Actuator_And_Sercurity_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorAndSercurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorAndSercurityDemoApplication.class, args);
+	}
+
+}
---
+++ Spring-Actuator-And-Sercurity-Demo/src/test/java/jrt/vku/spring/Spring_Actuator_And_Sercurity_Demo/SpringActuatorAndSercurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jrt.vku.spring.Spring_Actuator_And_Sercurity_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorAndSercurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
